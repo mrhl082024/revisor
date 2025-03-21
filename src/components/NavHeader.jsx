763907177ab169c769570acc5522eb350b805db7
mrhl082024 <mrhl082024@gmail.com>
2025-03-22 00:08:20 +0100

    generic styling for the generic company

--- src/components/NavHeader.jsx
+++ src/components/NavHeader.jsx
@@ -1,16 +1,19 @@
 import genericLogo from "/images/generic-logo.png";
+import "../styles/NavHeader.css";
 
 function NavHeader() {
   return (
     <>
-      <div>
-        <img id="navheader-logo" src={genericLogo} alt="" />
-      </div>
-      <input
-        id="company-search"
-        type="text"
-        placeholder="search for name or org.nr"
-      />
+      <nav id="nav">
+        <div id="navheader-logo">
+          <img src={genericLogo} alt="" />
+        </div>
+        <input
+          id="company-search"
+          type="text"
+          placeholder="search for name or org.nr"
+        />
+      </nav>
     </>
   );
 }
