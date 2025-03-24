ba6cb6a952663a5af8d9b20a3997a150013220b8
mrhl082024 <mrhl082024@gmail.com>
2025-03-24 10:12:26 +0100

    ADD AdvancedInfo

--- src/components/AdvancedInfo.jsx
+++ src/components/AdvancedInfo.jsx
@@ -1,22 +1,45 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "./ContextWindow";
 
 function AdvancedInfo() {
   const { company, setCompany } = useContext(Context);
+  const [show, setShow] = useState(false);
+
   console.log(company);
+
   return (
     <>
       {company.map((data, id) => (
         <div key={id}>
           <section>
-            Board Members:
+            <button
+              onClick={() => {
+                setShow((e) => !e);
+              }}
+            >
+              Show Board Members:
+            </button>
+            {show ? (
+              <ul>
+                {data.BoardMembers.map((boardmembers, id) => (
+                  <div key={id}>
+                    <p>Member {id + 1} </p>
+                    <li>Name: {boardmembers.Name} </li>
+                    <li>Board Member ID: {boardmembers.BoardMemberId} </li>
+                    <li>Since :{boardmembers.Since} </li>
+                  </div>
+                ))}
+              </ul>
+            ) : (
+              false
+            )}
+          </section>
+          <section>
             <ul>
-              {data.BoardMembers.map((boardmembers, id) => (
+              {data.IndustryCodes.map((industryCodes, id) => (
                 <div key={id}>
-                  <p>Member {id + 1} </p>
-                  <li>Name: {boardmembers.Name} </li>
-                  <li>Board Member ID: {boardmembers.BoardMemberId} </li>
-                  <li>Since :{boardmembers.Since} </li>
+                  <li>Industry Code: {industryCodes.IndustryCode} </li>
+                  <li>Industry Code ID: {industryCodes.IndustryCodeId} </li>
                 </div>
               ))}
             </ul>
