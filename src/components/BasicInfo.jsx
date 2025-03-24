1eee4113ec16d2199d1267c6fc859877cc9401f6
mrhl082024 <mrhl082024@gmail.com>
2025-03-24 09:34:11 +0100

    ADD BasicInfo

--- src/components/BasicInfo.jsx
+++ src/components/BasicInfo.jsx
@@ -10,11 +10,23 @@ const BasicInfo = () => {
       {company === null || company === undefined
         ? null
         : company.map((data, id) => (
-            <>
-              <h1>{data.CompanyName} </h1>
-              <p>{data.CompanyID}</p>
-              <p>{data.OrganisationNumber} </p>
-            </>
+            <div key={id} id="basicinfo">
+              <section>
+                <h1>{data.CompanyName} </h1>
+              </section>
+              <section>
+                <p>Company ID: {data.CompanyID}</p>
+                <p>Org.nr: {data.OrganisationNumber} </p>
+                <p>Current Employees: {data.CurrentEmployees} </p>
+              </section>
+              <section>
+                <p>{data.RegisteredAddress.City} </p>
+                <p>{data.RegisteredAddress.Country} </p>
+                <p>{data.RegisteredAddress.StreetName} </p>
+                <p>{data.RegisteredAddress.StreetNumber} </p>
+                <p>{data.RegisteredAddress.PostalCode} </p>
+              </section>
+            </div>
           ))}
     </>
   );
