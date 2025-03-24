705b1deb9ac7ddd7c3a0abc1a85451dc2b4ee72b
mrhl082024 <mrhl082024@gmail.com>
2025-03-24 09:44:26 +0100

    ADD BasicInfo

--- src/components/BasicInfo.jsx
+++ src/components/BasicInfo.jsx
@@ -13,18 +13,33 @@ const BasicInfo = () => {
             <div key={id} id="basicinfo">
               <section>
                 <h1>{data.CompanyName} </h1>
-                <p>Company ID: {data.CompanyID}</p>
-                <p>Org.nr: {data.OrganisationNumber} </p>
-                <p>Current Employees: {data.CurrentEmployees} </p>
+                <ul>
+                  <li>Company ID: {data.CompanyID}</li>
+                  <li>Org.nr: {data.OrganisationNumber}</li>
+                  <li>Current Employees: {data.CurrentEmployees}</li>
+                </ul>
               </section>
               <section>
                 Registered Address:
                 <ul>
                   <li>City: {data.RegisteredAddress.City} </li>
                   <li>Country: {data.RegisteredAddress.Country} </li>
-                  <li>Street Name:{data.RegisteredAddress.StreetName} </li>
-                  <li>Street Number:{data.RegisteredAddress.StreetNumber} </li>
+                  <li>Street Name: {data.RegisteredAddress.StreetName} </li>
+                  <li>Street Number: {data.RegisteredAddress.StreetNumber} </li>
                   <li>Postal Code: {data.RegisteredAddress.PostalCode} </li>
+                </ul>
+              </section>
+              <section>
+                Contact Info:
+                <ul>
+                  <li>Contact Info Id: {data.ContactInfo.ContactInfoId}</li>
+                  <li>Email: {data.ContactInfo.Email}</li>
+                  <li>Phone: {data.ContactInfo.Phone} </li>
+                  <li>
+                    <a href={data.ContactInfo.WebsiteUrl} target="_blank">
+                      {data.ContactInfo.WebsiteUrl}
+                    </a>
+                  </li>
                 </ul>
               </section>
             </div>
