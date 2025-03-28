581d94ac65e72c8549545b8d5fd7bca2fc46bd41
mrhl082024 <mrhl082024@gmail.com>
2025-03-28 10:47:26 +0100

    ADD compnaylist

--- src/components/CompanyList.jsx
+++ src/components/CompanyList.jsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { Context } from "./ContextWindow.jsx";
 import { useNavigate } from "react-router-dom";
 import Footer from "./Footer.jsx";
@@ -9,10 +9,33 @@ import companies from "../assets/Companies.json";
 
 const CompanyList = () => {
   const { company, setCompany } = useContext(Context);
+  const [pageCounter, setPageCounter] = useState(1);
+
+  function pageDecrement() {
+    if (pageCounter <= 1) {
+      setPageCounter(1);
+    } else {
+      setPageCounter((prev) => prev - 1);
+    }
+  }
+  function pageIncrement() {
+    setPageCounter((prev) => prev + 1);
+  }
 
   const navigate = useNavigate();
   return (
     <>
+      <section id="paginering">
+        <button
+          onClick={() => {
+            pageDecrement();
+          }}
+        >
+          Prev Page
+        </button>
+        <p>{pageCounter} </p>
+        <button onClick={pageIncrement}>Next page</button>
+      </section>
       <div id="company-list">
         {companies.map((entry, id) => (
           <button
